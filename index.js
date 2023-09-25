507a241ccc69e829de5526613df4948ca8a97f0a
Hadeel Obaid <hadeelobaid71@gmail.com>
2023-09-25 15:05:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+let multiply = num1*num2;
+
+let random = (Math.ceil (Math.random()+1));
+
+const num3 = 16;
+const num4 = 6;
+let mod =num3 % num4;
+
+let max = Math.max (1,2,3,4,5,6,7,8,9,10,20,19,18.17,16,15,14,13,12,11);
